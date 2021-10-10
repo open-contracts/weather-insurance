a2e12dcc7e445642cffc661bee1f9bb6b6c60a21
Krishnan Srinivasan <krshna@stanford.edu>
2021-10-10 15:43:48 -0700

    pack.sh makes packages.tar to include in contract

--- pack.sh
+++ pack.sh
@@ -1,2 +1,6 @@
-pip install --download ...
+#!/bin/sh
+# TODO: Enforce pip3 version to ensure that what is downloaded from pypi is reproducible
+mkdir packages/
+pip3 download --destination-directory packages -r requirements.txt
+tar -cvf packages.tar packages/
 # create pack.tar or something
