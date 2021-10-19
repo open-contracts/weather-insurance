2696527b09eb9f9ab385df4e035101d0371543c2
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-10-18 17:24:13 -0700

    Update pack.sh

--- dependencies/pack.sh
+++ dependencies/pack.sh
@@ -3,7 +3,7 @@
 mkdir -p oracle_package/pip
 pip3 install wheel
 pip3 wheel --wheel-dir oracle_package/pip -r requirements.txt
-cp oracle.py oracle_package/
+cp ../oracle.py oracle_package/
 cp domain_whitelist.txt oracle_package/
 zip -r oracle_package.zip oracle_package/
 rm -rf ./oracle_package
