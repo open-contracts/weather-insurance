b877b2dd16f63dcabf282aa886bcd14e07df8904
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-10-18 17:25:47 -0700

    Update and rename pack.sh to bundle_oracle.sh

--- dependencies/pack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# TODO: Enforce pip3 version to ensure that what is downloaded from pypi is reproducible
-mkdir -p oracle_package/pip
-pip3 install wheel
-pip3 wheel --wheel-dir oracle_package/pip -r requirements.txt
-cp ../oracle.py oracle_package/
-cp domain_whitelist.txt oracle_package/
-zip -r oracle_package.zip oracle_package/
-rm -rf ./oracle_package
