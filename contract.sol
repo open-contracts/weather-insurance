45124255f72610a1068b0f392ebac133afcfab08
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-10-18 20:01:20 -0700

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.8.3;
-import "@openzeppelin/contracts/OpenContractAlpha.sol"
+import "@opencontracts/contracts/OpenContractAlpha.sol"
 
 contract ExampleContract is OpenContractAlpha("e6295f30c2eb24805ed2a1568935a2db73e0d33e26b2a12a84fbad908251b458") {
     
