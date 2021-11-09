4617b8d63d02dd5855e9302f38b45083eca94f8c
Jonas Metzger <metzgerj@stanford.edu>
2021-11-09 00:17:42 -0800

    updated oracle

---
+++ pack_wheels.sh
@@ -0,0 +1,7 @@
+cd rain_oracle
+mkdir -p pip_wheels
+pip3 install wheel
+pip3 wheel --wheel-dir pip_wheels -r requirements.txt
+tar -czvf - pip_wheels | split -b 32M - pip_wheels.tar.gz
+rm -rf ./pip_wheels
+cat $(find . -type f | sort) | sha256sum | awk '{print $1}'
