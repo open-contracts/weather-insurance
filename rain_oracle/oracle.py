2bf5de99191a89cc01e92d652d90875c01b4c443
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-23 16:51:22 -0800

    Update oracle.py

--- rain_oracle/oracle.py
+++ rain_oracle/oracle.py
@@ -7,7 +7,7 @@ with opencontracts.enclave_backend() as enclave:
   beneficiary = enclave.user_input('Beneficiary')
   os.environ["CMR_USERNAME"] = enclave.user_input('Username for NASA Earthdata API')
   os.environ["CMR_PASSWORD"] = enclave.user_input('Password for NASA Earthdata API')
-  lat, lon = enclave.user_input('Latitude, Longitude (+-90, +-180)').split(',')
+  lat, lon = enclave.user_input('Latitude, Longitude (+-70, +-180)').split(',')
   lat, lon = float(lat), float(lon)
   
   yr, mo = enclave.user_input('Year-Month (YYYY-MM)').split('-')
@@ -25,7 +25,7 @@ with opencontracts.enclave_backend() as enclave:
   f = h5py.File('./dl/'+os.listdir('./dl')[0],'r')
   data = f['Grid']['surfacePrecipitation'][:, :]
   data[data<0] = float('nan')
-  precipitation = data[round((lon+180)/360*1440), round((lat+90)/180*720)]
+  precipitation = data[round((lon+180)/360*1440), round((lat+70)/140*720)]
   enclave.print(f'Validated Precipitation of {precipitation} on {yr}-{mo} at ({lat},{lon})')
   enclave.submit(beneficiary, int(precipitation, 1000), yr, mo, int(lat*1000), int(lon*1000),
                  types=('address', 'uint8', 'uint8', 'uint8', 'uint8', 'uint8'), function_name='claim')
