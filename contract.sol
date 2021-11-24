adf6849c32c7b8d451d2c9ad8e0c5da4d3c90110
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-23 16:47:32 -0800

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -10,44 +10,44 @@ contract WeatherInsurance is OpenContractAlpha {
     mapping(bytes32 => uint8) _threshold;
     mapping(bytes32 => bool) public insured;
 
-    function policyNo(address beneficiary, int8 lon, int8 lat, uint8 year, uint8 month) public pure returns(bytes32){
+    function policyID(address beneficiary, int8 lon, int8 lat, uint8 year, uint8 month) public pure returns(bytes32){
         return keccak256(abi.encodePacked(beneficiary, lon, lat, year, month));
     }
 
     function request(int8 lon, int8 lat, uint8 year, uint8 month, uint8 threshold, uint256 payout) public payable {
-        bytes32 policyNo = keccak256(abi.encodePacked(msg.sender, lon, lat, year, month));
-        _payment[policyNo] = msg.value;
-        _threshold[policyNo] = threshold;
-        _payout[policyNo] = payout;
+        bytes32 policyID = keccak256(abi.encodePacked(msg.sender, lon, lat, year, month));
+        _payment[policyID] = msg.value;
+        _threshold[policyID] = threshold;
+        _payout[policyID] = payout;
     }
     
     function retract(int8 lon, int8 lat, uint8 year, uint8 month) public {
-        bytes32 policyNo = keccak256(abi.encodePacked(msg.sender, lon, lat, year, month));
-        uint256 payment = _payment[policyNo];
-        _payment[policyNo] = 0;
+        bytes32 policyID = keccak256(abi.encodePacked(msg.sender, lon, lat, year, month));
+        uint256 payment = _payment[policyID];
+        _payment[policyID] = 0;
         payable(msg.sender).transfer(payment);
     }
     
     function provide(address beneficiary, int8 lon, int8 lat, uint8 year, uint8 month) public payable {
-        bytes32 policyNo = keccak256(abi.encodePacked(beneficiary, lon, lat, year, month));
-        require(msg.value >= _payout[policyNo], "You did not provide enough funds to provide the insurance.");
-        insured[policyNo] = true;
-        uint256 payment = _payment[policyNo];
-        _payment[policyNo] = 0;
-        _provider[policyNo] = msg.sender;
+        bytes32 policyID = keccak256(abi.encodePacked(beneficiary, lon, lat, year, month));
+        require(msg.value >= _payout[policyID], "You did not provide enough funds to provide the insurance.");
+        insured[policyID] = true;
+        uint256 payment = _payment[policyID];
+        _payment[policyID] = 0;
         payable(msg.sender).transfer(payment);
+        _provider[policyID] = msg.sender;
     }
     
     function claim(bytes32 oracleHash, address msgSender, address beneficiary, uint8 rainfall, int8 lon, int8 lat, uint8 year, uint8 month)
     public _oracle(oracleHash, msgSender, this.claim.selector) {
-        bytes32 policyNo = keccak256(abi.encodePacked(beneficiary, lon, lat, year, month));
-        require(insured[policyNo], "This insurance was not provided.");
-        uint256 payout = _payout[policyNo];
-        _payout[policyNo] = 0;
-        if (rainfall < _threshold[policyNo]) {
+        bytes32 policyID = keccak256(abi.encodePacked(beneficiary, lon, lat, year, month));
+        require(insured[policyID], "This insurance was not provided.");
+        uint256 payout = _payout[policyID];
+        _payout[policyID] = 0;
+        if (rainfall < _threshold[policyID]) {
             payable(beneficiary).transfer(payout);
         } else {
-            payable(_provider[policyNo]).transfer(payout);
+            payable(_provider[policyID]).transfer(payout);
         }
     }
 }
