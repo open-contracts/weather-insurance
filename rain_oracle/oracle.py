a8090402d382089f792883570a659f77ccd21797
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-30 20:10:21 -0800

    Update oracle.py

--- rain_oracle/oracle.py
+++ rain_oracle/oracle.py
@@ -4,14 +4,16 @@ from earthdata import Auth, DataGranules, DataCollections, Accessor
 from datetime import datetime
 
 with opencontracts.enclave_backend() as enclave:
-  os.environ["CMR_USERNAME"] = enclave.user_input('Username for NASA Earthdata API:')
-  os.environ["CMR_PASSWORD"] = enclave.user_input('Password for NASA Earthdata API:')
   lat, lon = enclave.user_input('Latitude, Longitude (+-70, +-180):').split(',')
   lat, lon = float(lat), float(lon)
-  
   yr, mo = enclave.user_input('Year-Month (YYYY-MM)').split('-')
-  yr, mo = int(yr), int(mo) 
+  yr, mo = int(yr), int(mo)
+  threshold = float(enclave.user_input('Threshold:'))
+  policyID = enclave.keccack(int(lat*1000), int(lon*1000), yr, mo, int(threshold*1000))
+  enclave.print(f'You are about to settle the insurance with policyID {policyID}.')
   
+  os.environ["CMR_USERNAME"] = enclave.user_input('Username for NASA Earthdata API:')
+  os.environ["CMR_PASSWORD"] = enclave.user_input('Password for NASA Earthdata API:')
   enclave.open_up_domain('nasa.gov')
   auth = Auth()
   auth.login(strategy='environment')
@@ -25,8 +27,11 @@ with opencontracts.enclave_backend() as enclave:
   data = f['Grid']['surfacePrecipitation'][:, :]
   data[data<0] = float('nan')
   precipitation = data[round((lon+180)/360*1440), round((lat+70)/140*720)]
-  enclave.print(f'Validated Precipitation of {precipitation} on {yr}-{mo} at ({lat},{lon})')
+  
+  damage_occured = precipitation < threshold
+  msg = f'Validated Precipitation of {precipitation} on {yr}-{mo} at ({lat},{lon}),'
+  msg += f'which means the damage did{' not'*(not damageOccured)} occur.'
+  enclave.print(msg)
   beneficiary = enclave.user_input('Address of Beneficiary:')
-  enclave.submit(beneficiary, int(precipitation, 1000), yr, mo, int(lat*1000), int(lon*1000),
-                 types=('address', 'uint8', 'uint8', 'uint8', 'uint8', 'uint8'), function_name='claim')
+  enclave.submit(beneficiary, policyID, damage_occured, types=('address', 'bytes32', 'bool'), function_name='settle')
                 
